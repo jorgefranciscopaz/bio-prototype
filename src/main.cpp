fb748bcd9ccb87abba0f2e678135f2d5a4e6030e
Jorge Paz <jorgefpaz011@gmail.com>
2025-06-25 10:36:45 -0600

    installed dependencies for python machine learning

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);  // Inicia comunicación serial
+  delay(1000);           // Espera para estabilizar conexión
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("✅ ESP32 funcionando correctamente.");
+  delay(1000);
 }
